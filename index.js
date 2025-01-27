f5c3c10d77b79ff97c0d3fecb01b994fcc8401b0
ZoesDev <6062364+ZoesDev@users.noreply.github.com>
2025-01-26 19:21:28 -0800

    added ignored users to the code and updated readme with a little bit about the config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { StreamerbotClient } from '@streamerbot/client';
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
+// List of usernames to ignore
+const ignoredUsers = process.env.ignoredUsers;
 
 // Create a new client with default options
 const client = new StreamerbotClient({
@@ -10,8 +12,7 @@ const client = new StreamerbotClient({
     subscribe: "*",
 });
 
-
-// Creating the connection to database
+// Creating the connection to the database
 const pool = mysql.createPool({
     host: process.env.mysqlHost,
     user: process.env.mysqlUser,
@@ -24,15 +25,41 @@ const pool = mysql.createPool({
     queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0,
-  });
+});
 
+// Subscription will automatically be added to the client with your listener function
+client.on('Twitch.ChatMessage', ({ data, timeStamp, event }) => {
+    const username = data.message.username;
 
-// Subscription will automatically be added to client with your listener function
-client.on('Twitch.ChatMessage', ({ data,timeStamp,event }) => {
+    // Check if the username is in the ignored list
+    if (ignoredUsers.includes(username.toLowerCase())) {
+        return; // Skip logging for ignored users
+    }
 
-    pool.query("INSERT INTO twitchChat (`source`, `timestamp`, `channel`, `userid`, `username`, `displayname`, `role`, `subscriber`, `subscribertier`, `monthssubscribed`,`hasbits`, `bits`, `ishighighted`, `isreply`,`fristmessage`, `returningchatter`, `message`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        [event.source,timeStamp,data.message.channel,data.message.userId,data.message.username,data.message.displayName,data.message.role,data.message.subscriber,data.message.subscriptionTier,data.message.monthsSubscribed,data.message.hasBits,data.message.bits,data.message.isHighlighted,data.isReply,data.firstMessage,data.message.returningChatter,data.message.message],
+    // Log the message to the database
+    pool.query(
+        "INSERT INTO twitchChat (`source`, `timestamp`, `channel`, `userid`, `username`, `displayname`, `role`, `subscriber`, `subscribertier`, `monthssubscribed`, `hasbits`, `bits`, `ishighighted`, `isreply`, `fristmessage`, `returningchatter`, `message`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+            event.source,
+            timeStamp,
+            data.message.channel,
+            data.message.userId,
+            data.message.username,
+            data.message.displayName,
+            data.message.role,
+            data.message.subscriber,
+            data.message.subscriptionTier,
+            data.message.monthsSubscribed,
+            data.message.hasBits,
+            data.message.bits,
+            data.message.isHighlighted,
+            data.isReply,
+            data.firstMessage,
+            data.message.returningChatter,
+            data.message.message,
+        ],
         function (error, results, fields) {
-                if (error) throw error;
-    });
-  });
+            if (error) throw error;
+        }
+    );
+});
